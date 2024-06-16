92c0518b3bfd9cf645ce3c6d189e07fa4168d74b
Taylor-Skomp <tjs5nx@virginia.edu>
2024-06-16 15:46:07 -0400

    Update Project Euler.py

--- Project Euler.py
+++ Project Euler.py
@@ -6,11 +6,14 @@ import numpy as np
 
 threes = np.arange(start=1,stop=1000,step=3)
 
-threes
+# essentially gives us an array of every multiple of 3 from 1-1000
 
 fives = np.arange(start=1,stop=1000,step=5)
 
-sum(threes,fives)
+# same process as before, except with multiples of 5
+
+# can then just find the total sum of each array and add the two values together to get the answer
+sum(sum(threes),sum(fives))
 
 
 # Problem 2
